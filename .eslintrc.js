98b58b231875803e35505bddcd38b1b02034716a
Giles Roadnight <10414642+Roaders@users.noreply.github.com>
2022-09-09 05:17:00 +0100

    setup linting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['dist/'],
+    rules: {
+        'prettier/prettier': ['error', { endOfLine: 'auto', tabWidth: 4, singleQuote: true, printWidth: 120 }],
+    },
+    overrides: [
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off',
+            },
+        },
+    ],
+};
