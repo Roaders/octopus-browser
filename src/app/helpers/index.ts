e1e866f4d303c3d8e16d7fb22efa2895f5a8774c
Roaders <10414642+Roaders@users.noreply.github.com>
2022-10-01 08:14:17 +0000

    Deploying to gh-pages from @ Roaders/octopus-browser@07443db44d7098f2ddd92d56b5603428e2af592b 🚀

--- src/app/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './chart.helper';
-export * from './compare.helper';
-export * from './selected-item.helper';
-export * from './type-guard.helpers';
-export * from './url.helper';
-export * from './vat.helper';
--- src/app/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './octopus-service';
-export * from './product-filter.service';
